0714b49c14803144f686b87d721156f370de7d50
David Dallas <Daviddallas606@gmail.com>
2024-03-17 10:31:46 +0000

    comments added to services file

--- src/Services/NearService.cs
+++ src/Services/NearService.cs
@@ -28,13 +28,15 @@ public class NearService
                 method_name = "ft_metadata",           
                 finality = "final", 
                 account_id = "absurd-pet.testnet",
-                args_base64 = "e30="
-                
+                args_base64 = "e30="                
             }
         };
 
-        //Convert to JSON
+        //Convert to JSON using NewtonSoft JSON lib
         var json = JsonConvert.SerializeObject(rpcPayload); 
+
+
+        //Format content for sending
         var content = new StringContent(json, Encoding.UTF8, "application/json");    
         
         //Sent request to NEAR
@@ -45,15 +47,17 @@ public class NearService
 
         //Convert response from array of ASCII characters into a string
 
-        var jsonResult = JObject.Parse(responseBody);
+        var jsonResult = JObject.Parse(responseBody);       
         var resultArray = jsonResult["result"]["result"].ToObject<byte[]>();
-        var resultString = Encoding.ASCII.GetString(resultArray);
+        var resultString = Encoding.ASCII.GetString(resultArray);      
         
         return resultString;
     }
 
     public async Task<string> GetFtBalanceAsync(string id)
     {
+
+        //Basically all of this logic can be stripped away into a helper function which can be called here and in method above. Leaving for now as just in prototyping phase. 
 
         string base64ConvertedString = Encoder.convertToBase64(id);
         Console.WriteLine(base64ConvertedString);
